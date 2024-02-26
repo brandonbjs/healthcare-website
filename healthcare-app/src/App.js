b288834fb81441a4429895ec2b1b40e40724387f
Brandon James Shaffer <bjs397@nau.edu>
2024-02-25 17:57:11 -0700

    Initial commit with project architecture set up for future project success.

---
+++ healthcare-app/src/App.js
@@ -0,0 +1,13 @@
+import NavigationBar from './components/NavigationBar'
+import './App.css'
+
+function App() {
+    return (
+        <div className="App">
+            <NavigationBar />
+            <h1>STU'S WEBSITE</h1>
+        </div>
+    )
+}
+
+export default App
