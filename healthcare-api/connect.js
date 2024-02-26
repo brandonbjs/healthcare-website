f94d826a94e04864221f26f3ac95c898b0b824e6
Brandon James Shaffer <bjs397@nau.edu>
2024-02-25 17:58:55 -0700

    Added connect.js

---
+++ healthcare-api/connect.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose'
+
+const serverName = '127.0.0.1'
+
+const databaseName = 'HealthcareWebsite'
+
+const URL = `mongodb://${serverName}:27017/${databaseName}`
+
+class Database {
+    constructor() {
+        this._connect()
+    }
+
+    _connect() {
+        mongoose
+            .connect(URL)
+            .then(() => console.log('database connection established'))
+            .catch((err) => console.log('error occured', err))
+    }
+}
+
+export default new Database()
